a17cf16211275c97724575a5d04f9ecb8cd3568d
davydocsurg <davydocsurg@gmail.com>
2023-02-10 10:21:36 +0100

    define category fillables and relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillables = [
+        'name',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
